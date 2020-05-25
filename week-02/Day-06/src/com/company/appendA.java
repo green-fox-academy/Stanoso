8272465691a9cdd77a71e30a46a6f2e4180b4446
Stanoso <standa.dostal@gmail.com>
2020-05-25 13:41:39 +0200

    Add appendA

---
+++ week-02/Day-06/src/com/company/appendA.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class appendA {
+
+    public static void main(String[] args) {
+
+        String[] animals = {"koal", "pand", "zebr"};
+        for (int i = 0; i < animals.length; i++) {
+            animals[i] += "a";
+            System.out.println(animals[i]);
+        }
+
+    }
+}
