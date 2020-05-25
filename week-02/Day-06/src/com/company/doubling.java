49aafa087bbe0d356bbeb183a7dda0d224d1df0e
Stanoso <standa.dostal@gmail.com>
2020-05-25 14:06:07 +0200

    Add doubling

---
+++ week-02/Day-06/src/com/company/doubling.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class doubling {
+
+    public static void main(String[] args) {
+        int baseNum = 123;
+        doubling(baseNum);
+
+    }
+    public static void doubling (int base) {
+        base *= 2;
+        System.out.println(base);
+    }
+}
