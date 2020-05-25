080b744d31652b429a1c51c051479572f87ae1db
Stanoso <standa.dostal@gmail.com>
2020-05-25 14:14:07 +0200

    Add greet

---
+++ week-02/Day-06/src/com/company/greet.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class greet {
+
+    public static void main(String[] args) {
+
+        String al = "Green Fox";
+        greet(al);
+    }
+
+    public static void greet(String name) {
+        System.out.println("Greetings dear, " + name + "!");
+    }
+}
