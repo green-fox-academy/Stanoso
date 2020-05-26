0babdecf00bf085a4682dcf356a4aac3ccdcdc2e
Stanoso <standa.dostal@gmail.com>
2020-05-26 11:46:30 +0200

    Add simpleReplace.java

---
+++ week-02/Day-07/src/com/company/simpleReplace.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class simpleReplace {
+
+    public static void main(String[] args) {
+
+        String example = "In a dishwasher far far away";
+        example = example.replace("dishwasher","galaxy");
+        System.out.println(example);
+
+    }
+}
