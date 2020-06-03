04e53228450ca6e7b9e8296e806a3ac8f1580733
Stanoso <standa.dostal@gmail.com>
2020-06-03 13:16:39 +0200

    Animal

---
+++ week-03/Day-13/src/Animal/Animal.java
@@ -0,0 +1,31 @@
+package Animal;
+
+public class Animal {
+
+    int hunger;
+    int thirst;
+
+    public Animal() {
+        this.hunger = 50;
+        this.thirst = 50;
+    }
+
+    public void eat() {
+        this.hunger--;
+        System.out.println("Hunger " + hunger);
+    }
+
+    public void drink() {
+        this.thirst--;
+        System.out.println("Thirst " + thirst);
+    }
+
+    public void play() {
+        this.hunger++;
+        this.thirst++;
+        System.out.println("Hunger " + hunger);
+        System.out.println("Thirst " + thirst);
+    }
+
+
+}
---
+++ week-03/Day-13/src/Animal/main.java
@@ -0,0 +1,19 @@
+package Animal;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Animal tiger = new Animal();
+
+        tiger.eat();
+        tiger.drink();
+        tiger.drink();
+        tiger.drink();
+        tiger.drink();
+        tiger.play();
+        tiger.play();
+
+    }
+
+}
