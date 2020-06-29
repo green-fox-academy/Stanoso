9981c734524ba2e8eb38ee263f526e4e0eff2088
Stanoso <standa.dostal@gmail.com>
2020-06-29 12:01:06 +0200

    Add Excercise3

---
+++ week-06/Day-01/src/Excercise3.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Excercise3 {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3, 9, 2, 8, 6, 5);
+        numbers.stream().filter((n) -> n*n > 20).forEach(System.out::println);
+    }
+}
