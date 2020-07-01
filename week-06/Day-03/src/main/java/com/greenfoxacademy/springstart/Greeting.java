f6ca695462cdf8ce34ce733ebe7b4e7ee523b372
Stanoso <standa.dostal@gmail.com>
2020-07-01 16:19:43 +0200

    Use atomicLong in helloRestController

--- week-06/Day-03/src/main/java/com/greenfoxacademy/springstart/Greeting.java
+++ week-06/Day-03/src/main/java/com/greenfoxacademy/springstart/Greeting.java
@@ -1,14 +1,26 @@
 package com.greenfoxacademy.springstart;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Greeting {
 
-    long id;
-    String content;
+    private long id;
+    private String content;
+
 
     public Greeting (long id, String content) {
         this.id = id;
         this.content = content;
     }
+
+    public Greeting (String content) {
+        this.id = 0;
+        this.content = content;
+
+    }
+
+
+
 
     public long getId() {
         return id;
--- week-06/Day-03/src/main/java/com/greenfoxacademy/springstart/controllers/helloRestController.java
+++ week-06/Day-03/src/main/java/com/greenfoxacademy/springstart/controllers/helloRestController.java
@@ -1,21 +1,23 @@
 package com.greenfoxacademy.springstart.controllers;
 
-import com.greenfoxacademy.springstart.AtomicLong;
 import com.greenfoxacademy.springstart.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class helloRestController {
 
-    private long counter=0;
+
+    private AtomicLong counter = new AtomicLong(1);
 
     @ResponseBody
     @RequestMapping (value = "/greeting")
-    public AtomicLong helloRest (String name) {
-        counter++;
-        AtomicLong hi = new AtomicLong(counter, "Hello, " + name + "!");
+    public Greeting helloRest (String name) {
+        Greeting hi = new Greeting(counter.longValue(), "Hello, " + name + "!");
+        counter.getAndIncrement();
         return hi;
     }
 
