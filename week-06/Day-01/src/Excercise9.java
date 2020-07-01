4db6cc007c351ad99341031ec4bfc0226bbcd3f0
Stanoso <standa.dostal@gmail.com>
2020-07-01 13:12:36 +0200

    Remove part of stream

--- week-06/Day-01/src/Excercise9.java
+++ week-06/Day-01/src/Excercise9.java
@@ -6,11 +6,11 @@ public class Excercise9 {
     public static void main(String[] args) {
 
         String text = "Mezi domama ma ma mama malou zahradku";
+
         text.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(c->1)))
+                .mapToObj(x -> (char) x)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(x ->1)))
                 .entrySet()
-                .stream()
                 .forEach(System.out::println);
 
     }
