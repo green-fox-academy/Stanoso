815c9f4492e5755977167aa5580069f73e25d37c
Stanoso <standa.dostal@gmail.com>
2020-07-08 13:32:07 +0200

    Add Printer

---
+++ week-07/Day-02/src/main/java/com/greenfoxacademy/dependencyexcercises/DependencyexcercisesApplication.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy.dependencyexcercises;
+
+import com.greenfoxacademy.dependencyexcercises.hellobeanworld.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyexcercisesApplication implements CommandLineRunner {
+
+    private Printer print;
+
+    @Autowired
+    DependencyexcercisesApplication(Printer print) {
+        this.print = print;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyexcercisesApplication.class, args);
+    }
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        print.log("Hello, what r ya doin?");
+    }
+}
---
+++ week-07/Day-02/src/test/java/com/greenfoxacademy/dependencyexcercises/DependencyexcercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.dependencyexcercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyexcercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
