dbff5d2249fafc051615c52d83917311b6703386
Stanoso <standa.dostal@gmail.com>
2020-08-11 18:07:25 +0200

    Refactoring

--- Project Phase/week 00/substring.js
+++ Project Phase/week 00/substring.js
@@ -1,14 +1,15 @@
-
 function subStr(stringToSearchIn, keyWord) {
-    if (keyWord.length <= stringToSearchIn.length) {
-    for (let i=keyWord.length; i<stringToSearchIn.length+1; i++) {
-        var sub = stringToSearchIn.substring((i-keyWord.length),i);
-        if (sub.localeCompare(keyWord)===0) {
-            return (i-keyWord.length);
-            }
-        }
+  if (keyWord.length <= stringToSearchIn.length) {
+    for (let i = keyWord.length; i < stringToSearchIn.length + 1; i++) {
+      var sub = stringToSearchIn.substring(i - keyWord.length, i);
+      if (sub.localeCompare(keyWord) === 0) {
+        return i - keyWord.length;
+      }
     }
-        return -1;
+  }
+  return -1;
 }
 
-console.log(subStr("Create a function that takes two strings as a parameter.", "o s"));
+console.log(
+  subStr("Create a function that takes two strings as a parameter.", "o s")
+);
