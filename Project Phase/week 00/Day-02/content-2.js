f909126e0edc55925f1e17bc4a3a46ebd7ddfaa1
Stanoso <standa.dostal@gmail.com>
2020-08-12 20:55:08 +0200

    Add content-2

---
+++ Project Phase/week 00/Day-02/content-2.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+//   <body>
+//     <ul>
+//       <li>placeholder</li>
+//       <li>placeholder</li>
+//       <li>placeholder</li>
+//       <li>placeholder</li>
+//     </ul>
+
+
+//       1) replace the list items' content with items from this list:
+//       ['apple', 'banana', 'cat', 'dog']
+//       2) change the <ul> element's background color to 'limegreen'
+//         - use css class to change the color instead of the style property
