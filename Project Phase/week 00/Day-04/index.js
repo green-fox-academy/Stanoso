3331d3da32779add3c4188942a40d71b69b3931f
Stanoso <standa.dostal@gmail.com>
2020-08-13 10:31:05 +0200

    Add app4

--- Project Phase/week 00/Day-04/index.js
+++ Project Phase/week 00/Day-04/index.js
@@ -1,20 +1,34 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Roadies!'
-    }
-  })
+  el: "#app",
+  data: {
+    message: "Hello Roadies!",
+  },
+});
 
-  var app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'You loaded this page on ' + new Date().toLocaleString() + ' Have a nice day!'
-    }
-  })
+var app2 = new Vue({
+  el: "#app-2",
+  data: {
+    message:
+      "You loaded this page on " +
+      new Date().toLocaleString() +
+      " Have a nice day!",
+  },
+});
 
-  var app3 = new Vue({
-    el: '#app-3',
-    data: {
-      seen: true
-    }
-  })
+var app3 = new Vue({
+  el: "#app-3",
+  data: {
+    seen: true,
+  },
+});
+
+var app4 = new Vue({
+  el: "#app-4",
+  data: {
+    todos: [
+      { text: "Learn JavaScript" },
+      { text: "Learn Vue" },
+      { text: "Build something awesome" },
+    ],
+  },
+});
