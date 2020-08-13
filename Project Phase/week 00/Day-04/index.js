e15d668f323b3bc77edcaf215b4fd6e19da45fd8
Stanoso <standa.dostal@gmail.com>
2020-08-13 10:27:29 +0200

    Add app3

--- Project Phase/week 00/Day-04/index.js
+++ Project Phase/week 00/Day-04/index.js
@@ -5,10 +5,16 @@ var app = new Vue({
     }
   })
 
-
   var app2 = new Vue({
     el: '#app-2',
     data: {
       message: 'You loaded this page on ' + new Date().toLocaleString() + ' Have a nice day!'
+    }
+  })
+
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
     }
   })
