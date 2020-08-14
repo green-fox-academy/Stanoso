c0bd95884192f6b1c9a930512e33fe92c5ddb440
Stanoso <standa.dostal@gmail.com>
2020-08-14 11:14:01 +0200

    Add ToDo app

---
+++ Project Phase/week 00/Day-05/todo-vue-app/src/components/Todo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+    <ul>
+        <li> Todo A </li> 
+        <li> Todo B </li> 
+        <li> Todo C </li> 
+    </ul> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  props: {
+    msg: String
+  }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
